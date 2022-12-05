89e4faba89948aca8ee9e3085f6f45ca66b1a7a0
rock374 <jackireton2@gmail.com>
2022-12-05 18:54:17 -0500

    removed terror room displays correct names

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const CHAT_HISTORY_PATH = "./" + CHAT_HISTORY_FNAME; /**< default chat history p
 
 /** GLOBALS */
 const usersList = new Map();
-const roomsList = ["global", "the mushroom", "pretty fly (for a fungi)", "domestic terrorism"];
+const roomsList = ["global", "the mushroom", "pretty fly (for a fungi)", "in the dirt"];
 let chatHistory;
 if (!(chatHistory = load_chat_history())) {
     chatHistory = {};
